7877934f9f1b2f37d77c7ed9576a7a96bf24e083
VeronikaVl <vlveronika61@gmail.com>
2022-06-03 20:05:05 -0400

    testHeaderH1

--- src/test/java/WebTest1.java
+++ src/test/java/WebTest1.java
@@ -31,5 +31,31 @@ String actualResult = welcome.getText(); //read text
     Assert.assertEquals(actualResult, expectedResult);
   driver.quit();//close driver after test
 
+
     }
+    @Test
+//TC_11_01 Подтвердите, что на странице по базовой ссылке в правом верхнем углу пользователь видит заголовок 99 Bottles of Beer
+//
+//Шаги:
+//1. Открыть вебсайт на базовой странице
+//2. Считать заголовок в правом верхнем углу
+//3. Подтвердить, что текст заголовка соответствует ожидаемому
+//4. Закрыть браузер
+    public void testHeaderH1 () throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/usr/local/bin/chromedriver";
+        String url = "https://www.99-bottles-of-beer.net/";
+        String expectedResult = "99 Bottles of Beer";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);//open main page
+        WebElement h1 = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='header']/h1"));
+        String actualResult = h1.getText(); //read text
+        Assert.assertEquals(actualResult, expectedResult);
+        driver.quit();//close driver after test
+
+
+    }
+
 }
