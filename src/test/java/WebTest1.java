f2696f8171d5bb76a5b0df1a6e973e0f391a7a31
VeronikaVl <vlveronika61@gmail.com>
2022-06-10 22:07:26 -0400

    Song99bottlesTest

--- src/test/java/WebTest1.java
+++ src/test/java/WebTest1.java
@@ -7,32 +7,34 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class WebTest1 {
-@Test
+    @Test
 
-    public void testMenuStartTypo () throws InterruptedException {
-String chromeDriver = "webdriver.chrome.driver";
-String driverPath = "/usr/local/bin/chromedriver";
-String url = "https://www.99-bottles-of-beer.net/";
-String expectedResult = "Welcome to 99 Bottles of Beer";
-System.setProperty(chromeDriver, driverPath);
-    WebDriver driver = new ChromeDriver();
+    public void testMenuStartTypo() throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/usr/local/bin/chromedriver";
+        String url = "https://www.99-bottles-of-beer.net/";
+        String expectedResult = "Welcome to 99 Bottles of Beer";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
 
-    driver.get(url);//open main page
-    WebElement menuBrowseLanguages =
-            driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']"));
-    //show path
-menuBrowseLanguages.click();//open element
- //   Thread.sleep(1000); //to pause
-    WebElement menuStart =
-            driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/']"));
-    menuStart.click();
-WebElement welcome = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/h2"));
-String actualResult = welcome.getText(); //read text
-    Assert.assertEquals(actualResult, expectedResult);
-  driver.quit();//close driver after test
+        driver.get(url);//open main page
+        WebElement menuBrowseLanguages =
+                driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']"));
+        //show path
+        menuBrowseLanguages.click();//open element
+        //   Thread.sleep(1000); //to pause
+        WebElement menuStart =
+                driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/']"));
+        menuStart.click();
+        WebElement welcome = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/h2"));
+        String actualResult = welcome.getText(); //read text
+        Assert.assertEquals(actualResult, expectedResult);
+        driver.quit();//close driver after test
 
 
     }
+
+
     @Test
 //TC_11_01 Подтвердите, что на странице по базовой ссылке в правом верхнем углу пользователь видит заголовок 99 Bottles of Beer
 //
@@ -41,7 +43,7 @@ String actualResult = welcome.getText(); //read text
 //2. Считать заголовок в правом верхнем углу
 //3. Подтвердить, что текст заголовка соответствует ожидаемому
 //4. Закрыть браузер
-    public void testHeaderH1 () throws InterruptedException {
+    public void testHeaderH1() throws InterruptedException {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/usr/local/bin/chromedriver";
         String url = "https://www.99-bottles-of-beer.net/";
@@ -57,6 +59,7 @@ String actualResult = welcome.getText(); //read text
 
 
     }
+
     @Test
 //TC_11_02 Подтвердите, что на странице по базовой ссылке последний пункт меню называется Submit new Language
 //
@@ -65,7 +68,7 @@ String actualResult = welcome.getText(); //read text
 //2. Считать название последнего пункта меню
 //3. Подтвердить, что название последнего пункта меню соответствует ожидаемому
 //4. Закрыть браузер
-    public void testMenuSubmitNewLanguage () throws InterruptedException {
+    public void testMenuSubmitNewLanguage() throws InterruptedException {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/usr/local/bin/chromedriver";
         String url = "https://www.99-bottles-of-beer.net/";
@@ -77,13 +80,14 @@ String actualResult = welcome.getText(); //read text
         WebElement menuSubmitNewLanguage =
                 driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/submitnewlanguage.html']"));
 
-       //menuSubmitNewLanguage.click();//open element
+        //menuSubmitNewLanguage.click();//open element
         String actualResult = menuSubmitNewLanguage.getText(); //read text
         Assert.assertEquals(actualResult, expectedResult);
         driver.quit();
 
 
     }
+
     @Test
     //TC_11_03 Подтвердите, что на странице по базовой ссылке последний пункт меню имеет подзаголовок Submit new Language
     //
@@ -92,7 +96,7 @@ String actualResult = welcome.getText(); //read text
     //2. Нажать на пункт меню Submit new Language
     //3. Считать название подзаголовка последнего пункта меню
     //4. Подтвердить, что название подзаголовка последнего пункта меню соответствует ожидаемому
-    public void testSubmitNewLanguageSubmenu () throws InterruptedException {
+    public void testSubmitNewLanguageSubmenu() throws InterruptedException {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/usr/local/bin/chromedriver";
         String url = "https://www.99-bottles-of-beer.net/";
@@ -110,13 +114,14 @@ String actualResult = welcome.getText(); //read text
         WebElement submenuSubmitNewLanguage =
                 driver.findElement(By.xpath
                         ("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='./submitnewlanguage.html']"
-                ));
+                        ));
         String actualResult = submenuSubmitNewLanguage.getText(); //read text
         Assert.assertEquals(actualResult, expectedResult);
         driver.quit();
 
 
     }
+
     @Test
 //TC_11_04 Подтвердите, что на странице по ссылке http://www.99-bottles-of-beer.net/abc.html , первый пункт подменю называется 0-9
 //
@@ -125,7 +130,7 @@ String actualResult = welcome.getText(); //read text
 //2. Считать название первого подзаголовка
 //3. Подтвердить, что название подменю соответствует ожидаемому
 //4. Закрыть браузер
-    public void testSubmenuBrowseLanguages () throws InterruptedException {
+    public void testSubmenuBrowseLanguages() throws InterruptedException {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/usr/local/bin/chromedriver";
         String url = "https://www.99-bottles-of-beer.net/";
@@ -148,18 +153,19 @@ String actualResult = welcome.getText(); //read text
 
 
     }
+
     @Test
 //TC_11_06 Подтвердите, что имена создателей сайта:
 //Oliver Schade
 //Gregor Scheithauer
 //Stefan Scheler
-    public void testSubmitNames () throws InterruptedException {
+    public void testSubmitNames() throws InterruptedException {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/usr/local/bin/chromedriver";
         String url = "https://www.99-bottles-of-beer.net/";
-        String expectedResult1= "Oliver Schade";
-        String expectedResult2= "Gregor Scheithauer";
-        String expectedResult3= "Stefan Scheler";
+        String expectedResult1 = "Oliver Schade";
+        String expectedResult2 = "Gregor Scheithauer";
+        String expectedResult3 = "Stefan Scheler";
         System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
 
@@ -168,7 +174,7 @@ String actualResult = welcome.getText(); //read text
                 driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='team.html']"));
 
         submenuTeam.click();
-       WebElement teamOliver =
+        WebElement teamOliver =
                 driver.findElement(By.xpath
                         ("//body/div[@id='wrap']/div[@id='main']/h3[contains(text(), 'Oliver Schade')]"
                         ));
@@ -191,47 +197,10 @@ String actualResult = welcome.getText(); //read text
 
     }
 
-  //  @Test
-//  Подтвердите, что если на странице по ссылке http://www.99-bottles-of-beer.net/submitnewlanguage.html ,  пользователь нажмет кнопку Submit Language,
-//  не заполнив информацию в обязательных полях, будет показана ошибка
-//
-//Error: Precondition failed - Incomplete Input.
-//
-//Шаги:
-//1. Открыть вебсайт на странице
-//2. Нажать на кнопку Submit Language
-//3. Подтвердить, что на странице показана ошибка
-//4. Подтвердить, что текст ошибки соответствует ожидаемому
-//5. Закрыть браузер
-
-   /* public void testSubmitLanguageError () {
-        String chromeDriver = "webdriver.chrome.driver";
-        String driverPath = "/usr/local/bin/chromedriver";
-        String url = "https://www.99-bottles-of-beer.net/";
-        String expectedResult = "Error: Precondition failed - Incomplete Input.";
-        System.setProperty(chromeDriver, driverPath);
-        WebDriver driver = new ChromeDriver();
-
-        driver.get(url);//open main page
-       WebElement menuSubmitNewLanguage =
-              driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/submitnewlanguage.html']"
-                ));
-
-        menuSubmitNewLanguage.click();
-
-               driver.findElement(By.xpath
-                      ("//input[@value='Submit Language']")).click();
-
-       // WebElement error = driver.findElement(By.xpath
-              //  ("//body/div[@id='wrap']/div[@id='main']/p[contains (text(), 'Precon')]/"
-                //));
-       // String actualResult = error.getText(); //read text
-        //Assert.assertEquals(actualResult, expectedResult);
-       // driver.quit();
 
 
-    } */
-   @Test
+
+    @Test
 //TC_11_13 Подтвердите, что на странице по ссылке http://www.99-bottles-of-beer.net/submitnewlanguage.html в первом пункте списка пользователь видит текст
 //
 //IMPORTANT: Take your time! The more carefully you fill out this form (especially the language name and description),
@@ -243,57 +212,58 @@ String actualResult = welcome.getText(); //read text
 //2. Считать текст
 //3. Подтвердить, что текст соответствует ожидаемому
 //4. Закрыть браузер
-   public void testFindTakeYourTime () throws InterruptedException {
-       String chromeDriver = "webdriver.chrome.driver";
-       String driverPath = "/usr/local/bin/chromedriver";
-       String url = "https://www.99-bottles-of-beer.net/";
-       String expectedResult1= "IMPORTANT: Take your time! The more carefully you fill out this form (especially the language name and description), " +
-               "the easier it will be for us and the faster your language will show up on this page. " +
-               "We don't have the time to mess around with fixing your descriptions etc. Thanks for your understanding.";
-
-       System.setProperty(chromeDriver, driverPath);
-       WebDriver driver = new ChromeDriver();
-
-       driver.get(url);//open main page
-       WebElement menuSubmitNewLanguage =
-               driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/submitnewlanguage.html']"
-               ));
-
-       menuSubmitNewLanguage.click();
-
-       WebElement textImortant =
-               driver.findElement(By.xpath
-                       (
-                               "//li[contains(., 'IMPORTANT')]"
-                       ));
-       String actualResult1 = textImortant.getText(); //read text
-       Assert.assertEquals(actualResult1, expectedResult1);
-
-       driver.quit();
-   }
-   @Test
-   //TC_11_14 Подтвердите, что нажав на пункт меню Browse Languages, пользователь
-   // увидит таблицу со следующими названиями для первого и второго столбцов:
-   //Language
-   //Author
-   //
-   //Шаги:
-   //1. Открыть вебсайт на базовой странице
-   //2. Нажать на пункт меню Browse Languages
-   //3. Считать названия первого и второго столбцов таблицы
-   //3. Подтвердить, что названия соответствует ожидаемым
-   //4. Закрыть браузер
-    public void testFindFirstSecondColumn () throws InterruptedException {
+    public void testFindTakeYourTime() throws InterruptedException {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/usr/local/bin/chromedriver";
         String url = "https://www.99-bottles-of-beer.net/";
-        String expectedResult1= "Language";
-       String expectedResult2= "Author";
+        String expectedResult1 = "IMPORTANT: Take your time! The more carefully you fill out this form (especially the language name and description), " +
+                "the easier it will be for us and the faster your language will show up on this page. " +
+                "We don't have the time to mess around with fixing your descriptions etc. Thanks for your understanding.";
+
         System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
 
         driver.get(url);//open main page
-       driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']")).click();
+        WebElement menuSubmitNewLanguage =
+                driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/submitnewlanguage.html']"
+                ));
+
+        menuSubmitNewLanguage.click();
+
+        WebElement textImortant =
+                driver.findElement(By.xpath
+                        (
+                                "//li[contains(., 'IMPORTANT')]"
+                        ));
+        String actualResult1 = textImortant.getText(); //read text
+        Assert.assertEquals(actualResult1, expectedResult1);
+
+        driver.quit();
+    }
+
+    @Test
+    //TC_11_14 Подтвердите, что нажав на пункт меню Browse Languages, пользователь
+    // увидит таблицу со следующими названиями для первого и второго столбцов:
+    //Language
+    //Author
+    //
+    //Шаги:
+    //1. Открыть вебсайт на базовой странице
+    //2. Нажать на пункт меню Browse Languages
+    //3. Считать названия первого и второго столбцов таблицы
+    //3. Подтвердить, что названия соответствует ожидаемым
+    //4. Закрыть браузер
+    public void testFindFirstSecondColumn() throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/usr/local/bin/chromedriver";
+        String url = "https://www.99-bottles-of-beer.net/";
+        String expectedResult1 = "Language";
+        String expectedResult2 = "Author";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);//open main page
+        driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']")).click();
 
         WebElement columnLanguage =
                 driver.findElement(By.xpath
@@ -303,13 +273,13 @@ String actualResult = welcome.getText(); //read text
 
         String actualResult1 = columnLanguage.getText(); //read text
         Assert.assertEquals(actualResult1, expectedResult1);
-       WebElement columnAuthor =
-               driver.findElement(By.xpath
-                       (
-                               "//th[contains(., 'Author')]"
-                       ));
-       String actualResult2 = columnAuthor.getText(); //read text
-       Assert.assertEquals(actualResult2, expectedResult2);
+        WebElement columnAuthor =
+                driver.findElement(By.xpath
+                        (
+                                "//th[contains(., 'Author')]"
+                        ));
+        String actualResult2 = columnAuthor.getText(); //read text
+        Assert.assertEquals(actualResult2, expectedResult2);
         driver.quit();
     }
 
@@ -318,11 +288,11 @@ String actualResult = welcome.getText(); //read text
     //
     //Шаги:
     //Придумать самостоятельно.
-    public void testFindNewComment () throws InterruptedException {
+    public void testFindNewComment() throws InterruptedException {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/usr/local/bin/chromedriver";
         String url = "https://www.99-bottles-of-beer.net/";
-        String expectedResult= "";
+        String expectedResult = "";
 
         System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
@@ -331,10 +301,10 @@ String actualResult = welcome.getText(); //read text
         driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/toplist.html']")).click();
 
 
-                driver.findElement(By.xpath
-                        (
-                                "//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='./newcomments.html']"
-                        )).click();
+        driver.findElement(By.xpath
+                (
+                        "//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='./newcomments.html']"
+                )).click();
 //<a href="./newcomments.html">New Comments</a>
         WebElement newComment =
                 driver.findElement(By.xpath
@@ -348,8 +318,47 @@ String actualResult = welcome.getText(); //read text
         driver.quit();
     }
 
+    @Test
+    //TC_11_21 Подтвердите, что на странице по ссылке http://www.99-bottles-of-beer.net/submitnewlanguage.html пользователь
+// видит предупреждение IMPORTANT:, написанное белыми буквами bold шрифтом на красном фоне, и что все буквы - capital
+    //
+    //Шаги:
+    //1. Открыть вебсайт на странице
+    //2. Считать слово IMPORTANT: из списка
+    //3. Подтвердить requirenments
+    //4. Закрыть браузер
+
+    public void testFindImportant() throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/usr/local/bin/chromedriver";
+        String url = "https://www.99-bottles-of-beer.net/";
+        String expectedResult1 = "IMPORTANT: Take your time! The more carefully you fill out this form (especially the language name and description), " +
+                "the easier it will be for us and the faster your language will show up on this page. " +
+                "We don't have the time to mess around with fixing your descriptions etc. Thanks for your understanding.";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);//open main page
+        WebElement menuSubmitNewLanguage =
+                driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/submitnewlanguage.html']"
+                ));
+
+        menuSubmitNewLanguage.click();
+
+        WebElement textImortant =
+                driver.findElement(By.xpath
+                        (
+                                "//li[contains(., 'IMPORTANT')]"
+                        ));
+        String actualResult1 = textImortant.getText(); //read text
+        Assert.assertEquals(actualResult1, expectedResult1);
+
+        driver.quit();
+    }
+
+
+
 }
-
-
 
 
